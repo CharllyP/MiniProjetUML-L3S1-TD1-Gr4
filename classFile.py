b7f9a6a0cb092b99561228b75a83d0897960fdb1
Charlly PIERRE <charlly.pierre@alumni.univ-avignon.fr>
2021-01-06 02:24:54 +0100

    rename class.py, implements main.py, map (non-generated), movement

---
+++ classFile.py
@@ -0,0 +1,315 @@
+import random
+import sys
+
+
+class Case:
+    ni = 0
+    mi = 0
+    type = ""
+
+
+class Map:
+    larg = 0
+    haut = 0
+    map = list()
+
+    def __init__(self, x, y):
+        self.larg = x
+        self.haut = y
+        self.map = list(range(x+1))
+        for i in range(x):
+            self.map[i] = list(range(y))
+
+    def routeMaker(self):
+        cpt = 0
+        return cpt
+
+    def generation(self):
+        for i in range(self.larg*self.haut):
+            rndn = random.randint(0, self.larg)
+            rndm = random.randint(0, self.haut)
+
+            if self.map[rndn][rndm].type == "":
+                if i == 0:
+                    self.map[rndn][rndm] = Maison(rndn, rndm)
+                elif i == 1:
+                    self.map[rndn][rndm] = Bibliotheque(rndn, rndm)
+                elif i == 2:
+                    self.map[rndn][rndm] = FastFood(rndn, rndm)
+                elif i == 3:
+                    self.map[rndn][rndm] = Universite(rndn, rndm)
+                elif i == 4:
+                    self.map[rndn][rndm] = Bar(rndn, rndm)
+                elif i == 5:
+                    i += self.routeMaker()
+                else:
+                    rndt = random.randint(1, 3)
+                    if rndt == 1:
+                        self.map[rndn][rndm] = Eau(rndn, rndm)
+                    if rndt == 2:
+                        self.map[rndn][rndm] = Foret(rndn, rndm)
+                    if rndt == 3:
+                        self.map[rndn][rndm] = Grisee(rndn, rndm)
+            else:
+                i -= 1
+
+
+class Maison(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "maison"
+
+    def actionMaison(self, heros):
+        heros.moral += 10
+        heros.satiete += 10
+        heros.hydratation += 10
+        chanceSoin = random.randint(1, 100)
+        chanceMaillot = random.randint(1, 100)
+
+        if heros.moral > heros.moralMax:
+            heros.moral = heros.moralMax
+        if heros.satiete > heros.satieteMax:
+            heros.satiete = heros.satieteMax
+        if heros.hydratation > heros.hydratationMax:
+            heros.hydratation = heros.hydratationMax
+        if heros.vie > heros.vieMax:
+            heros.vie = heros.vieMax
+        if chanceMaillot > 95:
+            heros.maillot = True
+        if chanceSoin > 95:
+            heros.vie += 10
+
+        if heros.malade:
+            heros.malade = False
+            heros.moral -= 20
+            heros.satiete -= 20
+            heros.hydratation -= 20
+
+
+class Bibliotheque(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "bibliotheque"
+
+    def actionBibliotheque(self, heros):
+        heros.moral += 20
+        chance = random.randint(1, 100)
+
+        if heros.moral > heros.moralMax:
+            heros.moral = heros.moralMax
+        if chance > 95:
+            heros.bonusDiplome += 10
+
+
+class FastFood(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "fastfood"
+
+    def actionFastFood(self, heros):
+        heros.moral += 10
+        heros.satiete += 25
+        heros.hydratation += 10
+        heros.vie -= 5
+
+        if heros.moral > heros.moralMax:
+            heros.moral = heros.moralMax
+        if heros.satiete > heros.satieteMax:
+            heros.satiete = heros.satieteMax
+        if heros.hydratation > heros.hydratationMax:
+            heros.hydratation = heros.hydratationMax
+
+
+class Universite(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "universite"
+
+    def actionUniversite(self, heros):
+        chance = random.randint(1, 100) + heros.bonusDiplome
+
+        if chance > 70:
+            heros.nbDiplome += 1
+            heros.moral += 5
+            if heros.moral > heros.moralMax:
+                heros.moral = heros.moralMax
+
+
+class Bar(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "bar"
+
+    def actionBar(self, heros):
+        heros.moral += 10
+        heros.hydratation += 25
+        heros.vie -= 5
+        chanceDiplome = random.randint(1, 100)
+        chanceMaillot = random.randint(1, 100)
+
+        if chanceDiplome > 95:
+            heros.bonusDiplome += 5
+        if chanceMaillot > 95:
+            heros.maillot = True
+        if heros.moral > heros.moralMax:
+            heros.moral = heros.moralMax
+        if heros.hydratation > heros.hydratationMax:
+            heros.hydratation = heros.hydratationMax
+
+
+class Route(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "route"
+
+    def piegeRoute(self, heros):
+        chancePiege = random.randint(1, 100)
+        if chancePiege > 95:
+            choixPiege = random.randint(1, 3)
+            if choixPiege == 1:   # feu rouge
+                heros.vie -= 1
+            if choixPiege == 2:   # Police
+                heros.moral -= 1
+                heros.nbArrestation += 1
+            if choixPiege == 3:   # Nid de poule
+                heros.hydratation -= 2
+                heros.satiete -= 2
+
+
+class Trottoir(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "trottoir"
+
+    def piegeTrottoir(self, heros):
+        chancePiege = random.randint(1, 100)
+        if chancePiege > 95:
+            choixPiege = random.randint(1,3)
+            if choixPiege == 1:   # Peau de banane
+                heros.vie -= 3
+            if choixPiege == 2:   # Poussette
+                heros.moral -= 2
+            if choixPiege == 3:   # Caca de chien
+                heros.satiete -= 1
+
+
+class Eau(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "eau"
+
+
+class Foret(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "foret"
+
+
+class Grisee(Case):
+    def __init__(self, ni, mi):
+        self.ni = ni
+        self.mi = mi
+        self.type = "grisee"
+
+
+class Heros:
+    pseudo = ""
+    nbDiplome = 0
+    type = ""
+    malade = False
+    maillot = False
+    vie = 100
+    vieMax = 100
+    hydratation = 100
+    hydratationMax = 100
+    satiete = 100
+    satieteMax = 100
+    moral = 100
+    moralMax = 100
+    bonusDiplome = 0
+    nbArrestation = 0
+    vehicule = ""
+    position = Case()
+    prevCase = Case()
+
+    def __init__(self, name, type):
+        self.pseudo = name
+        self.type = type
+        if type == "standard":
+            self.vie = self.vieMax = 75
+            self.hydratation = self.hydratationMax = 75
+            self.satiete = self.satieteMax = 75
+            self.moral = self.moralMax = 75
+        if type == "hippie":
+            self.vie = self.vieMax = 75
+            self.hydratation = self.hydratationMax = 50
+            self.satiete = self.satieteMax = 50
+            self.moral = self.moralMax = 100
+        if type == "hommePresse":
+            self.vie = self.vieMax = 100
+            self.hydratation = self.hydratationMax = 75
+            self.satiete = self.satieteMax = 75
+            self.moral = self.moralMax = 50
+
+    def maladie(self):
+        chance = random.randint(1, 100)
+
+        if chance > 90:
+            self.malade = True
+
+    def mourir(self):
+        if self.vie <= 0 or self.hydratation <= 0 or self.satiete <= 0 or self.moral <= 0 or self.nbArrestation >= 3:
+            print("t mor mdr")                                                                                          #A FAIRE
+
+
+class Etat:
+    etatheros = Heros
+    map = Case
+
+    def __init__(self, heros, map):
+        self.etatheros = heros
+        self.map = map
+
+    def sauvegarder(self, heros, map, numeroSave):
+        fichierSave = open("save"+str(numeroSave)+".txt", "w")
+        fichierSave.write(heros.pseudo+"\n")
+        fichierSave.write(heros.nbDiplome+"\n")
+        fichierSave.write(heros.type+"\n")
+        fichierSave.write(heros.malade+"\n")
+        fichierSave.write(heros.maillot+"\n")
+        fichierSave.write(heros.vie+"\n")
+        fichierSave.write(heros.vieMax+"\n")
+        fichierSave.write(heros.hydratation+"\n")
+        fichierSave.write(heros.hydratationMax+"\n")
+        fichierSave.write(heros.satiete+"\n")
+        fichierSave.write(heros.satieteMax+"\n")
+        fichierSave.write(heros.moral+"\n")
+        fichierSave.write(heros.moralMax+"\n")
+        fichierSave.write(heros.bonusDiplome+"\n")
+        fichierSave.write(heros.nbArrestation+"\n")
+        fichierSave.write(heros.vehicule+"\n")
+        fichierSave.write(heros.position.ni+"\n")
+        fichierSave.write(heros.position.mi+"\n")
+        fichierSave.write(heros.position.type+"\n")
+        fichierSave.write(heros.prevCase.ni+"\n")
+        fichierSave.write(heros.prevCase.mi+"\n")
+        fichierSave.write(heros.prevCase.type+"\n")
+        fichierSave.write(str(len(map))+","+str(len(map[0]))+"\n")
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                fichierSave.write(map[i][j].ni+",")
+                fichierSave.write(map[i][j].mi+",")
+                fichierSave.write(map[i][j].type+"\n")
+
+    def sauvegarderEtQuitter(self, heros, map, numeroSave):
+        self.sauvegarder(heros, map, numeroSave)
+        sys.exit(0)
---
+++ main.py
@@ -0,0 +1,98 @@
+import pygame
+import sys
+import classFile
+
+class Grille:
+    def __init__(self, ecran):
+        self.ecran = ecran
+        self.lignes = [((40, 0), (40, 600)), ((80, 0), (80, 600)), ((120, 0), (120, 600)), ((160, 0), (160, 600)),
+                       ((200, 0), (200, 600)), ((240, 0), (240, 600)), ((280, 0), (280, 600)), ((320, 0), (320, 600)),
+                       ((360, 0), (360, 600)), ((400, 0), (400, 600)), ((440, 0), (440, 600)),
+                       ((0, 40), (440, 40)), ((0, 80), (440, 80)), ((0, 120), (440, 120)), ((0, 160), (440, 160)),
+                       ((0, 200), (440, 200)), ((0, 240), (440, 240)), ((0, 280), (440, 280)), ((0, 320), (440, 320)),
+                       ((0, 360), (440, 360)), ((0, 400), (440, 400)), ((0, 440), (440, 440))]
+
+    def afficher(self):
+        for ligne in self.lignes :
+            pygame.draw.line(self.ecran, (25,25,25), ligne[0],ligne[1],1)
+
+class Jeu :
+    def __init__(self):
+        self.ecran = pygame.display.set_mode((600, 480))
+        pygame.display.set_caption('Quand on arrive en ville')
+        self.jeuEnCours = True
+        self.grille = Grille(self.ecran)
+
+    def mainFonction(self):
+        perso = pygame.image.load("perso40x40u.png").convert_alpha()
+        perso_x = 240
+        perso_y = 240
+        while self.jeuEnCours:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        perso = pygame.image.load("perso40x40u.png").convert_alpha()
+                        if perso_y >= 40:
+                            perso_y -= 40
+                    if event.key == pygame.K_DOWN:
+                        perso = pygame.image.load("perso40x40d.png").convert_alpha()
+                        if perso_y <= 400:
+                            perso_y += 40
+                    if event.key == pygame.K_LEFT:
+                        perso = pygame.image.load("perso40x40l.png").convert_alpha()
+                        if perso_x >= 40:
+                            perso_x -= 40
+                    if event.key == pygame.K_RIGHT:
+                        perso = pygame.image.load("perso40x40r.png").convert_alpha()
+                        if perso_x <= 360:
+                            perso_x += 40
+
+            self.ecran.fill((50,150,50))                                            # foret
+
+            pygame.draw.rect(self.ecran, (0, 150, 150), (0, 0, 360, 40))            # eau du haut
+            pygame.draw.rect(self.ecran, (0, 150, 150), (360, 0, 40, 400))          # eau de droite
+            pygame.draw.rect(self.ecran, (0, 150, 150), (0, 0, 80, 240))            # eau de gauche 1
+            pygame.draw.rect(self.ecran, (0, 150, 150), (40, 200, 80, 80))          # eau de gauche 2
+            pygame.draw.rect(self.ecran, (0, 150, 150), (80, 240, 80, 160))         # eau de gauche 3
+
+            pygame.draw.rect(self.ecran, (50, 50, 50), (80, 40, 40, 120))           # grisée haut gauche 1
+            pygame.draw.rect(self.ecran, (50, 50, 50), (40, 80, 120, 40))           # grisée haut gauche 2
+            pygame.draw.rect(self.ecran, (50, 50, 50), (160, 280, 80, 80))          # grisée milieu
+            pygame.draw.rect(self.ecran, (50, 50, 50), (40, 360, 80, 80))           # grisée bas carre
+            pygame.draw.rect(self.ecran, (50, 50, 50), (280, 440, 80, 40))          # grisée bas
+            pygame.draw.rect(self.ecran, (50, 50, 50), (400, 200, 40, 160))         # grisée droite
+            pygame.draw.rect(self.ecran, (50, 50, 50), (280, 120, 40, 40))          # grisée droite petit
+
+            pygame.draw.rect(self.ecran, (150, 150, 150), (320, 120, 40, 120))      # trottoir droite
+            pygame.draw.rect(self.ecran, (150, 150, 150), (160, 200, 40, 80))       # trottoir gauche
+            pygame.draw.rect(self.ecran, (150, 150, 150), (200, 240, 40, 40))       # trottoir gauche suite
+            pygame.draw.rect(self.ecran, (150, 150, 150), (240, 280, 40, 160))      # trottoir milieu
+            pygame.draw.rect(self.ecran, (150, 150, 150), (160, 400, 80, 40))       # trottoir bas
+            pygame.draw.rect(self.ecran, (150, 150, 150), (280, 360, 40, 40))       # trottoir bas droite
+
+            pygame.draw.rect(self.ecran, (0, 0, 0), (240, 80, 80, 40))
+            pygame.draw.rect(self.ecran, (0, 0, 0), (240, 120, 40, 120))
+            pygame.draw.rect(self.ecran, (0, 0, 0), (200, 160, 40, 40))
+            pygame.draw.rect(self.ecran, (0, 0, 0), (280, 240, 80, 40))
+            pygame.draw.rect(self.ecran, (0, 0, 0), (320, 280, 40, 80))
+
+            pygame.draw.rect(self.ecran, (220, 220, 220), (440, 0, 160, 480))       # interface de droite
+
+            pygame.draw.rect(self.ecran, (200, 0, 0), (240, 240, 40, 40))           # maison
+            pygame.draw.rect(self.ecran, (0, 0, 100), (320, 80, 40, 40))            # bibliotheque
+            pygame.draw.rect(self.ecran, (0, 255, 255), (120, 400, 40, 40))         # universite
+            pygame.draw.rect(self.ecran, (255, 255, 0), (160, 160, 40, 40))         # Fast-Food
+            pygame.draw.rect(self.ecran, (100, 0, 100), (320, 360, 40, 40))         # bar
+
+            self.grille.afficher()
+
+            self.ecran.blit(perso, (perso_x, perso_y))
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    pygame.init()
+    Jeu().mainFonction()
+    pygame.quit()
